7bb33652d9137ab79ea7968ada80782a19f59b5b
Philip Kirkbride <kirkins@gmail.com>
2017-04-15 11:53:37 -0400

    install vim plugins on start

--- .puppet/modules/vim/files/.vimrc
+++ .puppet/modules/vim/files/.vimrc
@@ -1,1 +1,16 @@
+set nocompatible              " be iMproved, required
+filetype off                  " required
+
+" set the runtime path to include Vundle and initialize
+set rtp+=/etc/vim/bundle/Vundle.vim
+call vundle#begin()
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'puppetlabs/puppet-syntax-vim'
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
+
 colorscheme pablo
--- .puppet/modules/vim/manifests/init.pp
+++ .puppet/modules/vim/manifests/init.pp
@@ -1,23 +1,29 @@
 class vim {
 
   Package { 'vim':
-    ensure => installed,
+    ensure      => installed,
   }
 
   file { '/etc/vim/vimrc':
-    source => 'puppet:///modules/vim/.vimrc',
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
+    source      => 'puppet:///modules/vim/.vimrc',
+    owner       => 'root',
+    group       => 'root',
+    mode        => '0644',
   }
 
   file { '/etc/vim/bundle/Vundle.vim':
-    ensure => 'directory',
-    source => 'puppet:///modules/vim/Vundle.vim',
-    recurse => 'remote',
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
+    ensure      => 'directory',
+    source      => 'puppet:///modules/vim/Vundle.vim',
+    recurse     => 'remote',
+    owner       => 'root',
+    group       => 'root',
+    mode        => '0644',
+  }
+
+  exec { 'load vim plugins':
+    command     => 'vim +PluginInstall +qall',
+    path        => '/usr/bin/:/bin/bash/',
+    environment => "HOME=/root"
   }
 
 }
