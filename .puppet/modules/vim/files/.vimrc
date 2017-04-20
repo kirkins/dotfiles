5b1cb9c6508e0c167cb5299fbdaca3ba72724a4e
Philip Kirkbride <phk@2klic.com>
2017-04-20 09:05:37 -0400

    change users vimrc only and not global in /etc

--- .puppet/modules/vim/files/.vimrc
+++ .puppet/modules/vim/files/.vimrc
@@ -2,7 +2,7 @@ set nocompatible              " required
 filetype off                  " required
 
 " set the runtime path to include Vundle and initialize
-set rtp+=/etc/vim/bundle/Vundle.vim
+set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
 
 " let Vundle manage Vundle, required
--- .puppet/modules/vim/manifests/init.pp
+++ .puppet/modules/vim/manifests/init.pp
@@ -18,13 +18,6 @@ class vim {
     mode        => '0644',
   }
 
-  file { '/etc/vim/vimrc':
-    source      => 'puppet:///modules/vim/.vimrc',
-    owner       => 'root',
-    group       => 'root',
-    mode        => '0644',
-  }
-
   file { '/etc/vim/bundle/Vundle.vim':
     ensure      => 'directory',
     source      => 'puppet:///modules/vim/Vundle.vim',
@@ -32,6 +25,15 @@ class vim {
     owner       => 'root',
     group       => 'root',
     mode        => '0644',
+  }
+
+  # iterate through homedirs in fact array
+  $facts['homedirs'].each |$homedir| {
+    # copy file to home directory
+    file { "/home/$homedir/.vimrc":
+      ensure => 'file',
+      source => 'puppet:///modules/vim/.vimrc',
+    }
   }
 
   exec { 'load vim plugins':
